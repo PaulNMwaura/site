fdda75296d9c2da85b2efea0136e3c47b2a21629
Paul Mwaura <paulmwaura555@gmail.com>
2024-11-03 23:30:50 -0500

    added place holder video, and new components to homepage

--- app/page.tsx
+++ app/page.tsx
@@ -33,14 +33,14 @@ export default function Home() {
           <p className="font-bold text-5xl text-gray-500 text-pretty">
             WANT TO CHANGE UP YOUR LOOK?
           </p>
-          <p className="font-sans text-3xl text-gray-500">
+          <p className="font-sans text-3xl text-purple-500">
             WE GOT YOU COVERED
           </p>
         </div>
       </div>
       <div className="w-full h-fit flex place-content-center">
         <div className="relative w-3/4 min-h-96 bg-slate-900 place-content-center text-center rounded-3xl">
-          <p className="font-bold text-3xl text-white">
+          <p className="font-bold text-3xl bg-gradient-to-l from-purple-500 to-orange-600 text-transparent bg-clip-text">
             MAKING IT EASIER TO: <br /> <br />
             FIND A STYLISTS NEAR YOU <br />
             VIEW PREVIOUS WORK DONE BY THEM <br />
@@ -49,26 +49,36 @@ export default function Home() {
         </div>
       </div>
       <div className="relative min-w-full min-h-fit flex flex-col items-center justify-center bg-white"> {/* {height/width fit content is very powerful} */}
-        <div className="w-fit h-fit font-bold text-3xl text-purple-500 text-center">
+      
+        <div className="pt-20 w-fit h-fit font-bold text-6xl text-purple-500 text-center">
           WHICH SERVICE ARE YOU LOOKING FOR?
         </div>
         <div className="w-[90%] h-1/2 bg-white justify-items-center grid grid-cols-1 md:grid-cols-2 lg:grid-cols-4 gap-20 p-10">
-          <div className="w-[80%] md:w-56 h-72 bg-red-500 flex items-center justify-center text-center font-bold text-white rounded-3xl">
-            <p> BARBERS</p>
-          </div>
+          <a href="." className="w-[80%] md:w-56 h-72 bg-red-500 flex flex-col place-content-end text-center font-bold text-white rounded-3xl">
+            <p>BARBERS</p>
+            <div className="w-full h-5/6 bg-emerald-700 rounded-ss-full">
+
+            </div>
+            
+          </a>
           <div className="w-[80%] md:w-56 h-72 bg-red-500 flex items-center justify-center text-center font-bold text-white rounded-3xl">
             <p>HAIR STYLERS</p>
           </div>
           <div className="w-[80%] md:w-56 h-72 bg-red-500 flex items-center justify-center text-center font-bold text-white rounded-3xl">
-            <p>BRAIDING</p>
+            <p>BRAIDERS</p>
           </div>
           <div className="w-[80%] md:w-56 h-72 bg-red-500 flex items-center justify-center text-center font-bold text-white rounded-3xl">
             <p>OTHER</p>
           </div>
         </div>
       </div>
-      <div className="relative min-w-full min-h-screen bg-black place-content-center text-center text-white">
-        A NEW SECTION 3
+      <div className="relative min-w-full min-h-fit bg-black place-content-center text-center text-white rounded-3xl">
+        <div className="video-container rounded-3xl">
+          <video autoPlay loop muted controls controlsList="nodownload" className="w-full h-full object-cover rounded-3xl">
+            <source src="/homePageVideo.mp4" type="video/mp4" />
+            Your browser does not support the video tag.
+          </video>
+        </div>
       </div>
       {/* <div className={styles.imageContainer}>
         <Image
